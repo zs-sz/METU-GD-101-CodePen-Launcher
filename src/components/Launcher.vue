f54f00a67a308f5ef38bc7a20e208e7b5ae7562e
b.sziklai@mito.hu <bsziklai@Sziklais-MacBook-Pro.local>
2018-06-04 13:57:40 +0200

    added dat.gui template

--- src/components/Launcher.vue
+++ src/components/Launcher.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="launcher">
-    <h2 class="launcher-title">P5.js Codepen Launcher</h2>
+    <h2 class="launcher-title">P5.js Codepen Template Generator</h2>
     <div class="presets">
       <preset
         :preset-config="defaultPresetSettings"
         preset-title="Basic Fixed Width"
-        preset-description= "500 x 500 Fixed size"
+        preset-description= "500 x 500 Fixed size, bare minimum"
       ></preset>
       <preset
         :preset-config="fluidPresetSettings"
         preset-title="Basic Fluid"
         preset-description="Fluid canvas and proper device pixel density (mobile or high ppi display)"
       ></preset>
+       <preset
+        :preset-config="datguiPresetSettings"
+        preset-title="Basic Fluid + DAT.gui"
+        preset-description="Fluid canvas w. dat.gui">
+      </preset>
     </div>
   </div>
 </template>
@@ -19,7 +24,8 @@
 <script>
 import Preset from './preset';
 import fluidP5 from '../fix-templates/p5-fluid.txt';
-import { DEFAULT_CODEPEN_SETTINGS } from '../constants.js'
+import datGui from '../fix-templates/p5-dat.txt';
+import { DEFAULT_CODEPEN_SETTINGS, P5JS_CDN_URL, DAT_GUI } from '../constants.js'
 import axios from 'axios';
 
 export default {
@@ -35,11 +41,22 @@ export default {
     },
     fluidPresetSettings () {
       let settings = {
-        js: fluidP5
+        js: fluidP5,
+        title: "P5 Fluid"
       }
 
       return JSON.stringify(Object.assign(this.defaultPreset, settings));
-    }
+    },
+    datguiPresetSettings () {
+      const resources = `${P5JS_CDN_URL};${DAT_GUI}`;
+      let settings = {
+        js: datGui,
+        js_external : resources,
+        title: "P5.js + dat.gui"
+      }
+      return JSON.stringify(Object.assign(this.defaultPreset, settings));
+    },
+
   },
   components: {
     Preset
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,11 @@
 const DEFAULT_CSS = require('./fix-templates/init.txt');
 const DEFAULT_JS = require('./fix-templates/p5-minimum.txt');
 
-export const P5JS_CDN_URL = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js';
+export const P5JS_CDN_URL = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/p5.js';
 export const DAT_GUI = 'https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.6.5/dat.gui.min.js';
+
 const DEFAULT_TITLE = 'P5 Launchpad Basic Template';
-const DEFAULT_DESCRIPTION = 'METU Generative Design 101 Course.';
+const DEFAULT_DESCRIPTION = 'P5js template generator';
 
 export const DEFAULT_CODEPEN_SETTINGS = {
   title              : DEFAULT_TITLE,
